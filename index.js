5ba7c8c8f21067a3a5afc1de654d8a13ec9a6293
A Hashim <ahashim983@yahoo.com>
2019-05-30 17:03:43 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(val => -val)
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(val => val*2)
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(val => val*val)
+}
+
+function reduceToTotal(sourceArray, startingPoint=0){
+    return sourceArray.reduce((partial_sum, a) => partial_sum + a, startingPoint);
+}
+
+function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+  
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
